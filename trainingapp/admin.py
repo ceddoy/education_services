31820954b294160fa4889b32fc2323946a1971a4
andrew <ceddoy@mail.ru>
2022-03-10 03:43:12 +0700

    добавил отображения inline answers

--- trainingapp/admin.py
+++ trainingapp/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from trainingapp.models import Topic, Question, Answer
 
 
+class AnswersInLine(admin.TabularInline):
+    extra = 1
+    model = Question.answer.through
+    readonly_fields = ('is_correct_answer',)
+
+    def is_correct_answer(self, instance):
+        return instance.answer.is_correct
+    is_correct_answer.short_description = 'Правильный ответ'
+
+
 class AnswerViewAdmin(admin.ModelAdmin):
     list_display = ('question', 'answer_text', 'is_correct')
     search_fields = ('question__id', 'question__topic__title')
@@ -10,9 +20,11 @@ class AnswerViewAdmin(admin.ModelAdmin):
 
 
 class QuestionViewAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
+    list_display = ('__str__', 'topic')
     search_fields = ('question__id', 'question__topic__title')
     raw_id_fields = ('topic',)
+    inlines = (AnswersInLine,)
+    fields = ('topic', 'question_text')
 
 
 class TopicViewAdmin(admin.ModelAdmin):
