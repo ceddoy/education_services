b9fbf21bfa278bf83cd2426f19494fee0409706a
andrew <ceddoy@mail.ru>
2022-03-09 13:14:19 +0700

    создал свой permission

---
+++ userapp/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+
+class ForNotAuthUserPermission(BasePermission):
+    """Авторизированный пользователь не может совершать регистрацию"""
+    def has_permission(self, request, view):
+        return not bool(request.user and request.user.is_authenticated)
