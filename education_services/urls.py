dcdd6557f9480996ddeb0898f72dc9d201f98125
andrew <ceddoy@mail.ru>
2022-03-09 18:01:10 +0700

    добавил пути для отображения вопросов

--- education_services/urls.py
+++ education_services/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from trainingapp.views import TopicListView
+from trainingapp.views import TopicRetrieveAPIView, TopicsListView, QuestionRetrieveAPIView
 from userapp.custom_auth_token import obtain_auth_token
 from userapp.views import CreateUserAPIView
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/clients/create/', CreateUserAPIView.as_view()),
     path('api_auth_token/', obtain_auth_token),
-    path('api/topics/', TopicListView.as_view()),
-    path('api/topics/<int:pk>/', TopicListView.as_view()),
+    path('api/topics/', TopicsListView.as_view()),
+    path('api/topics/<int:pk>/', TopicRetrieveAPIView.as_view()),
+    path('api/test/<int:pk>/', QuestionRetrieveAPIView.as_view()),
 ]
