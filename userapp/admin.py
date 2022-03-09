135c3c27aaaef8d35d7dc3f68dc82aee168aa69f
andrew <ceddoy@mail.ru>
2022-03-09 12:48:47 +0700

    подключил User к админ-панели, а также добавил хеширование пароля

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from userapp.models import User
+
+
+class UserViewAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            orig_obj = User.objects.get(pk=obj.pk)
+            if obj.password != orig_obj.password:
+                obj.set_password(obj.password)
+            obj.save()
+        else:
+            obj.set_password(obj.password)
+            super().save_model(request, obj, form, change)
+
+
+admin.site.register(User, UserViewAdmin)
