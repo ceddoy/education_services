fe93214d93854612c3a0e15d8ed1b3a136555f50
andrew <ceddoy@mail.ru>
2022-03-09 13:07:11 +0700

    создал кастомный authtoken серилизатор через email

---
+++ userapp/custom_auth_token.py
@@ -0,0 +1,36 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.compat import coreapi, coreschema
+from rest_framework.schemas import ManualSchema
+
+from userapp.serializer import CustomAuthTokenSerializer
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+    if coreapi is not None and coreschema is not None:
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="email",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Email",
+                        description="Valid email for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Password",
+                        description="Valid password for authentication",
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
+
+
+obtain_auth_token = CustomObtainAuthToken.as_view()
