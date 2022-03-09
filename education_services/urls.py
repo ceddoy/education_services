f41f97f9277b572df563fac81f6cd426cd4a6271
andrew <ceddoy@mail.ru>
2022-03-09 12:53:08 +0700

    добавил путь для создания нового пользователя

--- education_services/urls.py
+++ education_services/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from userapp.views import CreateUserAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/clients/create/', CreateUserAPIView.as_view()),
 ]
