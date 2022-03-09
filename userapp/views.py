c5a3541e7511b74429413ec96c95788d7a3fa8ed
andrew <ceddoy@mail.ru>
2022-03-09 12:51:42 +0700

    добавил view для создания User`a

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from userapp.models import User
+from userapp.serializer import UserModelSerializer
+
+
+class CreateUserAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
