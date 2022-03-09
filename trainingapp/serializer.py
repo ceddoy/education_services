486b117d592cdb96e9f9f3728f99bb9d1025950d
andrew <ceddoy@mail.ru>
2022-03-09 17:59:55 +0700

    добавил серилизатор для отображения вопроса и вариантов ответа

--- trainingapp/serializer.py
+++ trainingapp/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from trainingapp.models import Topic
+from trainingapp.models import Topic, Answer, Question
 
 
 class TopicsModelSerializer(serializers.ModelSerializer):
@@ -15,3 +15,23 @@ class TopicModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         fields = '__all__'
+
+
+class AnswersForQuestionModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('id', 'answer_text',)
+
+
+class QuestionModelSerializer(serializers.ModelSerializer):
+    answers = serializers.SerializerMethodField('get_answers_for_question')
+
+    @staticmethod
+    def get_answers_for_question(obj):
+        answers_list = Answer.objects.filter(question=obj.pk)
+        serializer = AnswersForQuestionModelSerializer(answers_list, many=True)
+        return serializer.data
+
+    class Meta:
+        model = Question
+        fields = ('question_text', 'answers')
