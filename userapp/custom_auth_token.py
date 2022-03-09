462b32f70498a7ebd4d247fa522c748bdbd21cc2
andrew <ceddoy@mail.ru>
2022-03-09 13:14:41 +0700

    подключил ForNotAuthUserPermission

--- userapp/custom_auth_token.py
+++ userapp/custom_auth_token.py
@@ -2,11 +2,13 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.compat import coreapi, coreschema
 from rest_framework.schemas import ManualSchema
 
+from userapp.permissions import ForNotAuthUserPermission
 from userapp.serializer import CustomAuthTokenSerializer
 
 
 class CustomObtainAuthToken(ObtainAuthToken):
     serializer_class = CustomAuthTokenSerializer
+    permission_classes = [ForNotAuthUserPermission, ]
     if coreapi is not None and coreschema is not None:
         schema = ManualSchema(
             fields=[
--- userapp/views.py
+++ userapp/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import CreateAPIView
 
 from userapp.models import User
+from userapp.permissions import ForNotAuthUserPermission
 from userapp.serializer import UserModelSerializer
 
 
 class CreateUserAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    permission_classes = [ForNotAuthUserPermission, ]
