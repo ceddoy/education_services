63b9ae90a65613177eaf05654b971becb4f254d3
andrew <ceddoy@mail.ru>
2022-03-09 13:06:51 +0700

    создал кастомный authtoken серилизатор через email

--- userapp/serializer.py
+++ userapp/serializer.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import password_validation
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import password_validation, authenticate
+from rest_framework import serializers
 
 from userapp.models import User
 
 
-class UserModelSerializer(ModelSerializer):
+class UserModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
@@ -20,3 +20,39 @@ class UserModelSerializer(ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField(
+        style={'input_type': 'email'},
+        label="Email",
+        write_only=True
+    )
+    password = serializers.CharField(
+        label="Password",
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+    token = serializers.CharField(
+        label="Token",
+        read_only=True
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),
+                                username=email, password=password)
+
+            if not user:
+                msg = 'Unable to log in with provided credentials.'
+                raise serializers.ValidationError(msg, code='authorization')
+        else:
+            msg = 'Must include "email" and "password".'
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
