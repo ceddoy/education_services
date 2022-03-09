df5fa8887bae753b501d709796a67c61cf46ab96
andrew <ceddoy@mail.ru>
2022-03-09 15:18:27 +0700

    добавил новые пути для работы с темами

--- education_services/urls.py
+++ education_services/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
+from trainingapp.views import TopicListView
 from userapp.custom_auth_token import obtain_auth_token
 from userapp.views import CreateUserAPIView
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/clients/create/', CreateUserAPIView.as_view()),
     path('api_auth_token/', obtain_auth_token),
+    path('api/topics/', TopicListView.as_view()),
+    path('api/topics/<int:pk>/', TopicListView.as_view()),
 ]
