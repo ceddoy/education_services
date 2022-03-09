0977bb4002d7d4be44b30f666ae34628cb811861
andrew <ceddoy@mail.ru>
2022-03-09 13:07:32 +0700

    добавил путь для создания токена

--- education_services/urls.py
+++ education_services/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from userapp.custom_auth_token import obtain_auth_token
 from userapp.views import CreateUserAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/clients/create/', CreateUserAPIView.as_view()),
+    path('api_auth_token/', obtain_auth_token),
 ]
