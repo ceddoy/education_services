20710e9734e61dc8de4b9a613e964cf495488978
andrew <ceddoy@mail.ru>
2022-03-12 21:29:01 +0700

    добавил в Topic отображение start_testing_url

--- trainingapp/serializer.py
+++ trainingapp/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from trainingapp.models import Topic, Answer, Question
+from trainingapp.services import get_next_page
 
 
 class TopicsModelSerializer(serializers.ModelSerializer):
@@ -11,10 +12,14 @@ class TopicsModelSerializer(serializers.ModelSerializer):
 
 
 class TopicModelSerializer(serializers.ModelSerializer):
+    start_testing_url = serializers.SerializerMethodField('get_page_begin_testing')
 
     class Meta:
         model = Topic
-        fields = '__all__'
+        fields = ("id", "title", 'description', 'start_testing_url')
+
+    def get_page_begin_testing(self, topic):
+        return get_next_page(Question.objects.filter(topic=topic).first().id)
 
 
 class AnswersForQuestionModelSerializer(serializers.ModelSerializer):
