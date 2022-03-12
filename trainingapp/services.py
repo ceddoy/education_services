b93d98189b5285491c725c545a55062d7c889cc5
andrew <ceddoy@mail.ru>
2022-03-13 01:15:55 +0700

    убрал create ResultAnswers и переместил в отдельную функцию обновление списка вопросов

--- trainingapp/services.py
+++ trainingapp/services.py
@@ -14,27 +14,25 @@ def get_correct_answers(question):
 def result_response(request, **kwargs):
     question = Question.objects.filter(id=kwargs.get('pk')).select_related('topic').first()
     correct_answers = get_correct_answers(question)
-    object_result = ResultAnswers.objects.filter(Q(user=request.user) & Q(topic=question.topic)).first()
-    if not object_result:
-        object_result = ResultAnswers.objects.create(user=request.user, topic=question.topic, list_questions=create_list_questions(question.topic))
-    object_result.list_questions.remove(kwargs.get('pk'))
-    object_result.save(update_fields=['list_questions'])
-    if object_result.list_questions:
-        url = get_next_page(random.choice(object_result.list_questions))
+    object_result_answers = check_user_for_retesting(request, question)
+    update_list_questions(object_result_answers, kwargs.get('pk'))
+    if object_result_answers.list_questions:
+        url = get_next_page(random.choice(object_result_answers.list_questions))
     if sorted(request.data.get("id")) == sorted([answer.get('id') for answer in correct_answers]):
-        object_result.count_correct_answer()
-        if not object_result.list_questions:
+        object_result_answers.count_correct_answer()
+        if not object_result_answers.list_questions:
             return {"result": 'Правильно, тест окончен',
-                    "result_answers": {"correct": object_result.count_right_answers,
-                                       "wrong": object_result.count_wrong_answers}}
+                    "result_answers": {"correct": object_result_answers.count_right_answers,
+                                       "wrong": object_result_answers.count_wrong_answers}}
         return {"result": 'Правильно',
                 'next_question': url}
     else:
-        object_result.count_wrong_answer()
-        if not object_result.list_questions:
+        object_result_answers.count_wrong_answer()
+        if not object_result_answers.list_questions:
             return {"result": 'Неправильно, тест окончен',
-                    "result_answers": {"correct": object_result.count_right_answers,
-                                       "wrong": object_result.count_wrong_answers}}
+                    "correct_answer": correct_answers,
+                    "result_answers": {"correct": object_result_answers.count_right_answers,
+                                       "wrong": object_result_answers.count_wrong_answers}}
         return {"result": 'Неправильно', "correct_answer": correct_answers, 'next_question': url}
 
 
@@ -47,8 +45,9 @@ def get_next_page(question_id):
 
 
 def check_user_for_retesting(request, question):
-    object_result_answers = ResultAnswers.objects.filter(Q(user=request.user) & Q(topic=question.topic)).first()
-    if object_result_answers is None:
-        return None
-    elif len(object_result_answers.list_questions) == 0:
-        return True
+    return ResultAnswers.objects.filter(Q(user=request.user) & Q(topic=question.topic)).first()
+
+
+def update_list_questions(object_result_answers, question_id):
+    object_result_answers.list_questions.remove(question_id)
+    object_result_answers.save(update_fields=['list_questions'])
