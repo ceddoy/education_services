d16af96105458298a42749c9d1648bc5238d2399
andrew <ceddoy@mail.ru>
2022-03-12 21:29:52 +0700

    добавил проверку на повторность теста

--- trainingapp/services.py
+++ trainingapp/services.py
@@ -1,3 +1,8 @@
+import random
+
+from django.db.models import Q
+from rest_framework.reverse import reverse
+
 from trainingapp.models import Question, ResultAnswers
 
 
@@ -9,18 +14,41 @@ def get_correct_answers(question):
 def result_response(request, **kwargs):
     question = Question.objects.filter(id=kwargs.get('pk')).select_related('topic').first()
     correct_answers = get_correct_answers(question)
-    object_result, created = ResultAnswers.objects.get_or_create(user=request.user, topic=question.topic)
+    object_result = ResultAnswers.objects.filter(Q(user=request.user) & Q(topic=question.topic)).first()
+    if not object_result:
+        object_result = ResultAnswers.objects.create(user=request.user, topic=question.topic, list_questions=create_list_questions(question.topic))
+    object_result.list_questions.remove(kwargs.get('pk'))
+    object_result.save(update_fields=['list_questions'])
+    if object_result.list_questions:
+        url = get_next_page(random.choice(object_result.list_questions))
     if sorted(request.data.get("id")) == sorted([answer.get('id') for answer in correct_answers]):
         object_result.count_correct_answer()
-        if question.topic.question_set.all().count() <= object_result.quantity_answers():
+        if not object_result.list_questions:
             return {"result": 'Правильно, тест окончен',
                     "result_answers": {"correct": object_result.count_right_answers,
                                        "wrong": object_result.count_wrong_answers}}
-        return {"result": 'Правильно'}
+        return {"result": 'Правильно',
+                'next_question': url}
     else:
         object_result.count_wrong_answer()
-        if question.topic.question_set.all().count() <= object_result.quantity_answers():
+        if not object_result.list_questions:
             return {"result": 'Неправильно, тест окончен',
                     "result_answers": {"correct": object_result.count_right_answers,
                                        "wrong": object_result.count_wrong_answers}}
-        return {"result": 'Неправильно', "correct_answer": correct_answers}
+        return {"result": 'Неправильно', "correct_answer": correct_answers, 'next_question': url}
+
+
+def create_list_questions(topic):
+    return [quest.id for quest in topic.question_set.all()]
+
+
+def get_next_page(question_id):
+    return reverse('question', args=[question_id])
+
+
+def check_user_for_retesting(request, question):
+    object_result_answers = ResultAnswers.objects.filter(Q(user=request.user) & Q(topic=question.topic)).first()
+    if object_result_answers is None:
+        return None
+    elif len(object_result_answers.list_questions) == 0:
+        return True
--- trainingapp/views.py
+++ trainingapp/views.py
@@ -1,9 +1,10 @@
 from django.http import JsonResponse
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
 from trainingapp.models import Topic, Question
 from trainingapp.serializer import TopicsModelSerializer, TopicModelSerializer, QuestionModelSerializer
-from trainingapp.services import result_response
+from trainingapp.services import result_response, check_user_for_retesting
 
 
 class TopicsListView(ListAPIView):
@@ -19,6 +20,13 @@ class TopicRetrieveAPIView(RetrieveAPIView):
 class QuestionRetrieveAPIView(RetrieveAPIView):
     serializer_class = QuestionModelSerializer
     queryset = Question.objects.all()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if check_user_for_retesting(request, instance):
+            return JsonResponse({"error": 'По данной теме, вы уже прошли тест!'})
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
         return JsonResponse(result_response(request, **kwargs))
