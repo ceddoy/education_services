c016f4b5f27bfb791651ff76a06ae614753588f7
andrew <ceddoy@mail.ru>
2022-03-13 16:42:37 +0700

    Изменил отображения данных пользователя, а также его история тестирования

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
+from trainingapp.admin import ResultAnswersInLine
 from userapp.models import User
 
 
 class UserViewAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
+    fields = ('email', 'password', 'is_active', 'is_staff', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    list_display = ('email', 'is_active', 'is_staff', 'created_at', 'updated_at')
+    search_fields = ('email',)
+    inlines = (ResultAnswersInLine,)
 
     def save_model(self, request, obj, form, change):
         if change:
