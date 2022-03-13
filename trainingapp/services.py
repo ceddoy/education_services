00298cc9fe33ba150e952038ee8ddcc41092556b
andrew <ceddoy@mail.ru>
2022-03-13 16:44:40 +0700

    добавил еще логику в функцию update_and_get_list_questions

--- trainingapp/services.py
+++ trainingapp/services.py
@@ -1,6 +1,7 @@
 import random
 
 from django.db.models import Q
+from rest_framework.exceptions import ValidationError
 from rest_framework.reverse import reverse
 
 from trainingapp.models import Question, ResultAnswers
@@ -11,11 +12,11 @@ def get_correct_answers(question):
     return [{"id": answer.get('id'), "answer_text": answer.get('answer_text')} for answer in answers]
 
 
-def result_response(request, **kwargs):
+def result_answer_question(request, **kwargs):
     question = Question.objects.filter(id=kwargs.get('pk')).select_related('topic').first()
     correct_answers = get_correct_answers(question)
     object_result_answers = check_user_for_retesting(request, question)
-    update_list_questions(object_result_answers, kwargs.get('pk'))
+    url = update_and_get_list_questions(object_result_answers, kwargs.get('pk'))
     if object_result_answers.list_questions:
         url = get_next_page(random.choice(object_result_answers.list_questions))
     if sorted(request.data.get("id")) == sorted([answer.get('id') for answer in correct_answers]):
@@ -48,6 +49,12 @@ def check_user_for_retesting(request, question):
     return ResultAnswers.objects.filter(Q(user=request.user) & Q(topic=question.topic)).first()
 
 
-def update_list_questions(object_result_answers, question_id):
-    object_result_answers.list_questions.remove(question_id)
-    object_result_answers.save(update_fields=['list_questions'])
+def update_and_get_list_questions(object_result_answers, question_id):
+    try:
+        object_result_answers.list_questions.remove(question_id)
+        object_result_answers.save(update_fields=['list_questions'])
+        if object_result_answers.list_questions:
+            return get_next_page(random.choice(object_result_answers.list_questions))
+        return None
+    except ValueError:
+        raise ValidationError({"error": 'По данной теме, вы уже прошли тест!'})
