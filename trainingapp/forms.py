98e85cb02673e696ead73b915029bb94be7b1a56
andrew <ceddoy@mail.ru>
2022-03-13 16:43:19 +0700

    Создал пользовательский QuestionAnswersFormSet для валидации

---
+++ trainingapp/forms.py
@@ -0,0 +1,11 @@
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+
+class QuestionAnswersFormSet(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        for answer in self.cleaned_data:
+            if answer.get('answer').is_correct:
+                return self.cleaned_data
+        raise ValidationError('Необходимо создать хотя бы 1 правильный ответ')
