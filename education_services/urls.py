3c5efd203b7134006248cb9e3ed66b075fdf19b3
andrew <ceddoy@mail.ru>
2022-03-14 02:11:22 +0700

    добавил name всем роутерам

--- education_services/urls.py
+++ education_services/urls.py
@@ -7,9 +7,9 @@ from userapp.views import CreateUserAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/clients/create/', CreateUserAPIView.as_view()),
-    path('api_auth_token/', obtain_auth_token),
-    path('api/topics/', TopicsListView.as_view()),
-    path('api/topics/<int:pk>/', TopicRetrieveAPIView.as_view()),
+    path('api/clients/create/', CreateUserAPIView.as_view(), name='create_user'),
+    path('api_auth_token/', obtain_auth_token, name='get_token'),
+    path('api/topics/', TopicsListView.as_view(), name='topics'),
+    path('api/topics/<int:pk>/', TopicRetrieveAPIView.as_view(), name='topic_detail'),
     path('api/test/<int:pk>/', QuestionRetrieveAPIView.as_view(), name='question'),
 ]
